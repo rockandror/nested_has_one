6a229b52afd49743fb61474754a1bd1bb8cd0b96
Senén Rodero Rodríguez <senenrodero@gmail.com>
2017-11-24 15:17:56 +0100

    Create organizations crud

---
+++ app/controllers/organizations_controller.rb
@@ -0,0 +1,47 @@
+class OrganizationsController < ApplicationController
+
+  def new
+    @organization = Organization.new
+  end
+
+  def edit
+    @organization = Organization.find(params[:id])
+  end
+
+  def index
+    @organizations = Organization.all
+  end
+
+  def create
+    @organization = Organization.new(organization_params)
+    if @organization.save
+      redirect_to organizations_path, notice: "Created successfully!"
+    else
+      flash.now[:alert] = "Create error"
+      render :new
+    end
+  end
+
+  def update
+    @organization = Organization.find(params[:id])
+    if @organization.update(organization_params)
+      redirect_to organizations_path, notice: "Updated successfully!"
+    else
+      flash.now[:alert] = "Update error"
+      render :edit
+    end
+  end
+
+  def destroy
+    @organization = Organization.find(params[:id])
+    @organization.destroy
+    redirect_to organizations_path, notice: "Removed successfully!"
+  end
+
+  private
+
+    def organization_params
+      params.require(:organization).permit(:name)
+    end
+
+end
